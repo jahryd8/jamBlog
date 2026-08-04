89243235b04550a985553786e1ec1e0e86a5eac9
jahryd8 <ryderjaheim8@gmail.com>
2026-08-04 17:37:27 -0500

    fix: varcel hosting

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173','https://jamblog.vercel.app'], // Vite frontend URL AND VARCEL deployment URL
+  origin: ['http://localhost:5173','https://jam-blog-rosy.vercel.app/'], // Vite frontend URL AND VARCEL deployment URL
   credentials: true
 }));
 app.use(express.json());
