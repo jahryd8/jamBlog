90f39e820719636eea62a3a095bef916e30d8f31
jahryd8 <ryderjaheim8@gmail.com>
2026-08-04 17:15:53 -0500

    chore: deployment

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite frontend URL
+  origin: ['http://localhost:5173','https://jamblog.vercel.app'], // Vite frontend URL AND VARCEL deployment URL
   credentials: true
 }));
 app.use(express.json());
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig } from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://jamblog.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
