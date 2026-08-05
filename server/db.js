56cfe4c53ac4742d20f1f0949b4b1b73dacc7f94
jahryd8 <ryderjaheim8@gmail.com>
2026-08-04 19:33:57 -0500

    fix: bug fixes

--- server/db.js
+++ server/db.js
@@ -6,14 +6,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// Log successful connection or error
 pool.on('connect', () => {
   console.log('Connected to PostgreSQL Database!');
 });
